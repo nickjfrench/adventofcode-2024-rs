b12f15a1cdf8f7dc442cb7c22b5d278548e93f4e
Nick French <64120654+nickjfrench@users.noreply.github.com>
2024-12-02 21:49:58 -0500

    Day 1 Part 2 Complete

--- src/puzzles/day1.rs
+++ src/puzzles/day1.rs
@@ -5,41 +5,41 @@ pub fn solve(input: String) {
 }
 
 fn part1(input: String) -> String {
-    const DEBUG: bool = false;
+    let (col1, col2) = get_columns_and_sort(&input);
 
-    let mut total = 0;
+    calc_total_distance(col1, col2).to_string()
+}
+
+fn part2(input: String) -> String {
+    let (col1, col2) = get_columns_and_sort(&input);
+    
+    calc_total_sim_score(col1, col2).to_string()
+}
+
+fn get_columns_and_sort(input: &str) -> (Vec<u32>, Vec<u32>) {
     let (mut col1, mut col2) = split_columns_u32(&input);
-
-    if DEBUG {
-        println!("DEBUG: col1 amt: {:?}", col1.len());
-        println!("DEBUG: col2 amt: {:?}", col2.len());
-    }
 
     col1.sort();
     col2.sort();
 
-    if col1.len() != col2.len() {
-        return "ERROR: Day 1 - Column 1 length does not match Column 2 length"
-            .to_string()
-    }
+    (col1, col2)
+}
 
-    if DEBUG {
-        println!("DEBUG: col1 sorted first : {:?}", col1[0]);
-        println!("DEBUG: col1 sorted last : {:?}", col1[col1.len() - 1]);
-        println!("DEBUG: col2 sorted first : {:?}", col2[0]);
-        println!("DEBUG: col2 sorted last : {:?}", col2[col2.len() - 1]);
+fn calc_row_distance(col1: u32, col2: u32) -> u32 {
+    // Find the largest, then calculate distance.
+    if col1 >= col2 {
+        col1 - col2
+    } else {
+        col2 - col1
     }
+}
 
+fn calc_total_distance(col1: Vec<u32>, col2: Vec<u32>) -> u32 {
+    let mut total = 0;
     for row_number in 0..col1.len() {
-        // Calculate Distance from Largest - Smallest.
-        if col1[row_number] >= col2[row_number] {
-            total += col1[row_number] - col2[row_number];
-        } else {
-            total += col2[row_number] - col1[row_number];
-        }
+        total += calc_row_distance(col1[row_number], col2[row_number]);
     }
-
-    total.to_string()
+    total
 }
 
 fn split_columns_u32(input: &str) -> (Vec<u32>, Vec<u32>) {
@@ -59,6 +59,18 @@ fn str_to_u32(s: &str) -> u32 {
     s.parse::<u32>().unwrap()
 }
 
-fn part2(input: String) -> String {
-    "not implemented.".to_string()
+fn count_instances(left: u32, col2: &[u32]) -> u32 {
+    col2.iter().filter(|x| **x == left).count() as u32
+}
+
+fn calc_sim_score(item: u32, count: u32) -> u32 {
+    item * count
+}
+
+fn calc_total_sim_score(col1: Vec<u32>, col2: Vec<u32>) -> u32 {
+    let mut total = 0;
+    for row in col1 {
+        total += calc_sim_score(row, count_instances(row, &col2))
+    }
+    total
 }
